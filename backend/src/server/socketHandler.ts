4c5583d071a1e890ca341279fdfa9df4679b7d67
Aditya <adityaguptavns1@gmail.com>
2026-04-03 19:26:23 +0530

    temporary matchmaking logic

--- backend/src/server/socketHandler.ts
+++ backend/src/server/socketHandler.ts
@@ -1,13 +1,48 @@
 import { WebSocket, WebSocketServer } from "ws";
-import { GameManager } from "./gameManager";
+import { GameManager } from "./gameManager.js";
 
 
-const wss = new WebSocketServer({port : 6969});
+const wss = new WebSocketServer({ port: 6969 });
 
 const gameManager = new GameManager();
 
-wss.on("connection", (socket : WebSocket) => {
-    const socketId = Math.random().toString();
-    console.log(socketId);
+const clients = new Map<string, WebSocket>();
+const queue: string[] = [];
+
+wss.on("connection", (socket: WebSocket) => {
+    const socketId = Math.random().toString(36).slice(2);
+    // console.log(socketId);
+
+    clients.set(socketId, socket);
+    console.log("connected: ", socketId);
+
+    socket.on("message", (data) => {
+        const message = JSON.parse(data.toString());
+
+        if (message.type == " MOVE") {
+            const move = message.playload.move;
+            //socketId ~ playerId
+            const result = gameManager.handleMove(socketId, move);
+
+            if (!result.success) {
+                socket.send(JSON.stringify(result));
+                return;
+            }
+        }
+
+        // for testing: auto-create game when 2 players connect
+        // will remove it once i add the elo match making logic
+        if (queue.length >= 2) {
+            const player1 = queue.shift()!;
+            const player2 = queue.shift()!;
+
+            const gameId = gameManager.createGame(player1, player2);
+
+            console.log("Game created:", gameId);
+        }
+
+
+        const gameId = gameManager.getGameState
+    })
 })
 
