28fbad221b1597d4062d207f34d2c46450ea356d
Aditya <adityaguptavns1@gmail.com>
2026-04-05 17:13:18 +0530

    added CompleteGame type for replay system

--- backend/src/server/types.ts
+++ backend/src/server/types.ts
@@ -3,3 +3,14 @@ export type Player = {
     rating : number;
 }
 
+export type CompletedGame = {
+  gameId: string;
+  players: Player[];
+  pgn: string;
+  moves: string[];
+  createdAt: number;
+  result: {
+    winner: string | null;
+    reason: string | null;
+  };
+};
