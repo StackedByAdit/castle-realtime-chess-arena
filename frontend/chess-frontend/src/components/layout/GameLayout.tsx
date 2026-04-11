3f9e2288403ef742929c1819bfad821f65b6551a
Aditya <adityaguptavns1@gmail.com>
2026-04-11 18:30:51 +0530

    added gameLayout

---
+++ frontend/chess-frontend/src/components/layout/GameLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function GameLayout({ children }: Props) {
+  return (
+    <div className="game-screen">
+      {children}
+    </div>
+  );
+}
